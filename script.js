9edff7b5cb9dc4a0842406af934992ff12adfe08
melcolmm <31358679+melcolmm@users.noreply.github.com>
2024-10-07 12:57:25 +0200

    Add reset button, restructure code

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const cont = document.querySelector("#container");
+const cont = document.querySelector("#drawPad");
+const btnReset = document.querySelector("#reset");
 
 const divArray = [];
+const startRowsColumns = 16;
+let numRows = startRowsColumns;
+let numCols = startRowsColumns;
+
+drawGrid();
+
+const elements = document.querySelectorAll(".drawBlock");
+const elementsFilled = document.querySelectorAll(".drawBlockFilled");
+
+elements.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        element.setAttribute("class", "drawBlockFilled");
+    })
+});
 
 function createRow(numCols) {
     const row = [];
@@ -12,25 +27,16 @@ function createRow(numCols) {
     return row;
 }
 
- const numRows = 16;
- const numCols = 16;
-
- for (let row = 0; row < numRows; row++) {
-    divArray.push(createRow(numCols));
- }
-
- for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-        divArray[row][col].setAttribute("id", row + "-" + col);
-        cont.appendChild(divArray[row][col]);
+function drawGrid() {
+    for (let row = 0; row < numRows; row++) {
+        divArray.push(createRow(numCols));
     }
- }
+    
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            divArray[row][col].setAttribute("id", row + "-" + col);
+            cont.appendChild(divArray[row][col]);
+        }
+    }
+}
 
-const elements = document.querySelectorAll(".drawBlock");
-const elementsFilled = document.querySelectorAll(".drawBlockFilled");
-
-elements.forEach((element) => {
-    element.addEventListener("mouseover", () => {
-        element.setAttribute("class", "drawBlockFilled");
-    })
-});
